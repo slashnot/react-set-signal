fe87cc529d1748ef04248c96682afddfdb5c8b12
Ramkumar K <slashnot84@gmail.com>
2025-11-24 20:32:35 +0530

    Bump version to 1.0.5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { useReactive } from "./hooks/useReactive";
 export { useReactiveSignal } from "./hooks/useReactiveSignal";
 export { createSignal } from "./utils/createSignal";
-export * from "@preact/signals-core";
+export { Computed, Effect, Signal, batch, computed, effect, signal, untracked } from "@preact/signals-core";
 export * from "mutative";
