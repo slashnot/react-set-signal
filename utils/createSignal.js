83f9903c167b1b2f4be89335e0c58810ac52d6de
Ramkumar K <slashnot84@gmail.com>
2025-11-25 03:09:00 +0530

    Fix createSignal return value is draft

--- utils/createSignal.js
+++ utils/createSignal.js
@@ -5,22 +5,26 @@ import { create, isDraft, rawReturn } from 'mutative'
 // Immer style signal state producer
 // ------------------------------------
 export const getSignalValue = ($signal, fnOrValue) => {
-  if (typeof fnOrValue === 'function') {
-    const mutationResponse = create($signal.value, (draft) => {
-      const result = fnOrValue(draft)
+    if (typeof fnOrValue === 'function') {
+        const mutationResponse = create($signal.value, (draft) => {
+            const result = fnOrValue(draft)
 
-      // fnOrValue is a funtion that used the draft
-      if (typeof result === 'undefined' || isDraft(result)) {
-        return draft
-      }
-      // fnOrValue is a funtion that returned a value, not used the draft
-      return typeof result === 'object' ? rawReturn(result) : result
-    })
-    // Return response when fnOrValues is a function
-    return mutationResponse
-  }
-  // Return value when fnOrValues is a value
-  return fnOrValue
+            // fnOrValue is a funtion that used the draft
+            if (isDraft(result)) {
+                return draft
+            }
+            // fnOrValue is a funtion that returned an object, not used the draft
+            if (typeof result === 'object' && !isDraft(result)) {
+                return rawReturn(result)
+            }
+
+            return draft
+        })
+        // Return response when fnOrValues is a function
+        return mutationResponse
+    }
+    // Return value when fnOrValues is a value
+    return fnOrValue
 }
 
 
@@ -28,16 +32,16 @@ export const getSignalValue = ($signal, fnOrValue) => {
 // Immer style signal setter
 // ------------------------------------
 export const setSignal = ($signal, fnOrValue) => {
-  $signal.value = getSignalValue($signal, fnOrValue)
-  return $signal.value
+    $signal.value = getSignalValue($signal, fnOrValue)
+    return $signal.value
 }
 
 // ------------------------------------
 // Better Signal creator
 // ------------------------------------
 export const createSignal = (initialValue) => {
-  const $signal = signal(initialValue)
-  $signal.set = (value) => setSignal($signal, value)
-  return $signal
+    const $signal = signal(initialValue)
+    $signal.set = (value) => setSignal($signal, value)
+    return $signal
 }
 // ------------------------------------
