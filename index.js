314b58dbaad1a81aace175efe931646602ce44af
Ramkumar K <slashnot84@gmail.com>
2025-11-25 03:00:20 +0530

    Removed mutative exports

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export { useReactive } from "./hooks/useReactive";
 export { useReactiveSignal } from "./hooks/useReactiveSignal";
 export { createSignal } from "./utils/createSignal";
 export { Computed, Effect, Signal, batch, computed, effect, signal, untracked } from "@preact/signals-core";
-export * from "mutative";
